39500764b12d291f757f5b5d159787d722840b75
Bảo Phạm <phamthaibao2410@gmail.com>
2024-11-16 00:55:28 +0700

    change

--- src/main/java/com/example/shopshoes/server/entity/base/IsIdentified.java
+++
@@ -1,6 +0,0 @@
-package com.example.shose.server.entity.base;
-
-public interface IsIdentified {
-
-    String getId();
-}
--- src/main/java/com/example/shopshoes/tool/ok.java
+++
@@ -1,4 +0,0 @@
-package com.example.shopshoes.tool;
-
-public class ok {
-}
