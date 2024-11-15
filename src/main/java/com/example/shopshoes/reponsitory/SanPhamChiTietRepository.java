6a9016a9dec353f1589c0833f4e5d00517cbb560
Bảo Phạm <phamthaibao2410@gmail.com>
2024-11-15 23:27:19 +0700

    change

--- src/main/java/com/example/shopshoes/reponsitory/SanPhamChiTietRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopshoes.reponsitory;
-
-import com.example.shopshoes.entity.SanPhamChiTiet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SanPhamChiTietRepository extends JpaRepository<SanPhamChiTiet,Integer> {
-}
